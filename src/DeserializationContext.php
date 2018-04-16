f95c99b7fd0bfd9d375050efa9d8580216282b39
Luís Cobucci <lcobucci@gmail.com>
2018-04-17 01:13:30 +0200

    Simplify the deserialization context (by a lot)
    Using the new abstractions we don't need to rely on PSR-7 objects,
    allowing the usage of this package in different contexts as well.

--- src/DeserializationContext.php
+++ src/DeserializationContext.php
@@ -1,66 +1,27 @@
 <?php
 declare(strict_types=1);
 
-namespace Lcobucci\Chimera\Serialization\Jms;
+namespace Lcobucci\Chimera\MessageCreator\JmsSerializer;
 
 use JMS\Serializer\DeserializationContext as BaseContext;
-use Lcobucci\Chimera\Routing\Attributes;
-use Psr\Http\Message\ServerRequestInterface;
-use Zend\Expressive\Router\RouteResult;
+use Lcobucci\Chimera\Input;
 
 final class DeserializationContext extends BaseContext
 {
-    public const ATTR_ROUTER_PARAMS = 'chimera.route_params';
-    public const ATTR_QUERY_PARAMS  = 'chimera.query_params';
-    public const ATTR_GENERATED_ID  = 'chimera.generated_id';
-
     /**
-     * @var ServerRequestInterface
+     * @var Input
      */
-    private $request;
+    private $input;
 
-    public function __construct(ServerRequestInterface $request)
+    public function __construct(Input $input)
     {
         parent::__construct();
 
-        $this->request = $request;
+        $this->input = $input;
     }
 
-    public static function fromRequest(ServerRequestInterface $request): self
+    public function getInput(): Input
     {
-        return self::appendRequestAttributes(new self($request), $request);
-    }
-
-    private static function appendRequestAttributes(
-        self $context,
-        ServerRequestInterface $request
-    ): DeserializationContext {
-        $context->setAttribute(self::ATTR_ROUTER_PARAMS, self::extractRouteParams($request))
-                ->setAttribute(self::ATTR_QUERY_PARAMS, $request->getQueryParams());
-
-        $generatedId = $request->getAttribute(Attributes::GENERATED_ID);
-
-        if ($generatedId !== null) {
-            $context->setAttribute(self::ATTR_GENERATED_ID, $generatedId);
-        }
-
-        return $context;
-    }
-
-    private static function extractRouteParams(ServerRequestInterface $request): array
-    {
-        /** @var RouteResult|null $routeResult */
-        $routeResult = $request->getAttribute(RouteResult::class);
-
-        if ($routeResult === null) {
-            return [];
-        }
-
-        return $routeResult->getMatchedParams();
-    }
-
-    public function getRequest(): ServerRequestInterface
-    {
-        return $this->request;
+        return $this->input;
     }
 }
